e941c5454498e3542afb05e2c8d4cf4f9daaa92e
Krystyna Kruchkovska <kruchok883@gmail.com>
2018-12-22 13:36:06 +0200

    Add new project

---
+++ Conflate/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Conflate
+//
+//  Created by Mac on 12/22/18.
+//  Copyright © 2018 CO.KrystynaKruchcovska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
