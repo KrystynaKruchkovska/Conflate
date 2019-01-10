134646a1e2a57e6167d9aa46e8419410aba563f9
pawelpeksa <pmpeksa@gmail.com>
2019-01-10 20:45:23 +0100

    fix round button on MapView

--- Conflate/View/FloatingActionBtn.swift
+++ Conflate/View/FloatingActionBtn.swift
@@ -11,12 +11,15 @@ import UIKit
 class FloatingActionBtn: UIButton {
 
     override func draw(_ rect: CGRect) {
-        
-        layer.cornerRadius = self.frame.height/2
+        layer.cornerRadius = self.frame.height/2.0
         layer.shadowOpacity = 0.25
         layer.shadowRadius = 5
         layer.shadowOffset = CGSize(width: 0, height: 10)
+        layer.backgroundColor = btnColor().cgColor
     }
-
-
+    
+    private func btnColor() -> UIColor {
+        return UIColor(red: 128/255, green: 105/255, blue: 194/255, alpha: 1.0)
+    }
+    
 }
